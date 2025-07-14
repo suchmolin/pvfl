982835fbf533eeb785350753225682bbd0b62de3
suchmolin11 <suchmolin11@gmail.com>
2025-07-14 18:55:31 -0400

    eliminar h-fit

--- src/components/Diferenciadores/index.jsx
+++ src/components/Diferenciadores/index.jsx
@@ -7,7 +7,7 @@ export default function Diferenciadores() {
                     <div className=" bg-[#97D700] p-2 rounded-lg w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] relative">
                         <img src="/img/PERSONAESTRELLA.webp" alt="persona estrella icono" className="w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] " />
                     </div>
-                    <div className="relative px-2 py-2 flex justify-center items-cente h-fit ">
+                    <div className="relative px-2 py-2 flex justify-center items-cente   ">
                         <img src="/img/fondoformablanco.webp" alt="forma fondo blanco" className="w-full h-full absolute top-0" />
                         <h3 className="relative">GUÍAS CERTIFICADOS</h3>
                     </div>
@@ -16,7 +16,7 @@ export default function Diferenciadores() {
                     <div className=" bg-[#001A70] p-2 rounded-lg w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] relative">
                         <img src="/img/FORMASGEOBLANCAS.webp" alt="persona estrella icono" className="w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] " />
                     </div>
-                    <div className="relative px-2 py-2 flex justify-center items-cente h-fit ">
+                    <div className="relative px-2 py-2 flex justify-center items-cente   ">
                         <img src="/img/fondoformablanco.webp" alt="forma fondo blanco" className="w-full h-full absolute top-0" />
                         <h3 className="relative">ACTIVIDADES RECREATIVAS</h3>
                     </div>
@@ -24,7 +24,7 @@ export default function Diferenciadores() {
                     <div className=" bg-[#FFA400] p-2 rounded-lg w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] relative">
                         <img src="/img/DOSPERSONAS.webp" alt="persona estrella icono" className="w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] " />
                     </div>
-                    <div className="relative px-2 py-2 flex justify-center items-cente h-fit ">
+                    <div className="relative px-2 py-2 flex justify-center items-cente   ">
                         <img src="/img/fondoformablanco.webp" alt="forma fondo blanco" className="w-full h-full absolute top-0" />
                         <h3 className="relative">CUPOS LIMITADOS</h3>
                     </div>
