52dfa1e18a7dcc0c25fb66295577b8de2ebabb3c
suchmolin11 <suchmolin11@gmail.com>
2025-07-14 18:17:09 -0400

    cambio forma img2

--- src/components/Diferenciadores/index.jsx
+++ src/components/Diferenciadores/index.jsx
@@ -4,7 +4,7 @@ export default function Diferenciadores() {
              min-[400px]:text-xl min-[460px]:text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl mt-5">
             <div className="flex flex-col gap-2">
                 <div className="flex gap-2 items-center">
-                    <div className=" bg-[#97D700] p-2 rounded-lg">
+                    <div className=" bg-[#97D700] p-2 rounded-lg w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] relative">
                         <img src="/img/PERSONAESTRELLA.webp" alt="persona estrella icono" className="w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] h-fit" />
                     </div>
                     <div className="relative px-2 py-2 flex justify-center items-cente h-fit ">
@@ -13,7 +13,7 @@ export default function Diferenciadores() {
                     </div>
                 </div>
                 <div className="flex gap-2 items-center">
-                    <div className=" bg-[#001A70] p-2 rounded-lg">
+                    <div className=" bg-[#001A70] p-2 rounded-lg w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] relative">
                         <img src="/img/FORMASGEOBLANCAS.webp" alt="persona estrella icono" className="w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] h-fit" />
                     </div>
                     <div className="relative px-2 py-2 flex justify-center items-cente h-fit ">
@@ -21,7 +21,7 @@ export default function Diferenciadores() {
                         <h3 className="relative">ACTIVIDADES RECREATIVAS</h3>
                     </div>
                 </div><div className="flex gap-2 items-center">
-                    <div className=" bg-[#FFA400] p-2 rounded-lg">
+                    <div className=" bg-[#FFA400] p-2 rounded-lg w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] relative">
                         <img src="/img/DOSPERSONAS.webp" alt="persona estrella icono" className="w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] h-fit" />
                     </div>
                     <div className="relative px-2 py-2 flex justify-center items-cente h-fit ">
