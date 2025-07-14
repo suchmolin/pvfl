df4ced4c89ca34ac61480e06a5f1f08d2d1b416a
suchmolin11 <suchmolin11@gmail.com>
2025-07-14 18:14:26 -0400

    cambio forma img

--- src/components/Diferenciadores/index.jsx
+++ src/components/Diferenciadores/index.jsx
@@ -1,12 +1,11 @@
 export default function Diferenciadores() {
-    const square = "w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] h-fit"
     return (
         <div className="w-full flex justify-center text-[#001A70] eastman-bold text-md
              min-[400px]:text-xl min-[460px]:text-2xl sm:text-3xl lg:text-4xl 2xl:text-5xl mt-5">
             <div className="flex flex-col gap-2">
                 <div className="flex gap-2 items-center">
                     <div className=" bg-[#97D700] p-2 rounded-lg">
-                        <img src="/img/PERSONAESTRELLA.webp" alt="persona estrella icono" className={square} />
+                        <img src="/img/PERSONAESTRELLA.webp" alt="persona estrella icono" className="w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] h-fit" />
                     </div>
                     <div className="relative px-2 py-2 flex justify-center items-cente h-fit ">
                         <img src="/img/fondoformablanco.webp" alt="forma fondo blanco" className="w-full h-full absolute top-0" />
@@ -15,7 +14,7 @@ export default function Diferenciadores() {
                 </div>
                 <div className="flex gap-2 items-center">
                     <div className=" bg-[#001A70] p-2 rounded-lg">
-                        <img src="/img/FORMASGEOBLANCAS.webp" alt="persona estrella icono" className={square} />
+                        <img src="/img/FORMASGEOBLANCAS.webp" alt="persona estrella icono" className="w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] h-fit" />
                     </div>
                     <div className="relative px-2 py-2 flex justify-center items-cente h-fit ">
                         <img src="/img/fondoformablanco.webp" alt="forma fondo blanco" className="w-full h-full absolute top-0" />
@@ -23,7 +22,7 @@ export default function Diferenciadores() {
                     </div>
                 </div><div className="flex gap-2 items-center">
                     <div className=" bg-[#FFA400] p-2 rounded-lg">
-                        <img src="/img/DOSPERSONAS.webp" alt="persona estrella icono" className={square} />
+                        <img src="/img/DOSPERSONAS.webp" alt="persona estrella icono" className="w-[40px] sm:w-[60px] lg:w-[80px] 2xl:w-[100px] h-fit" />
                     </div>
                     <div className="relative px-2 py-2 flex justify-center items-cente h-fit ">
                         <img src="/img/fondoformablanco.webp" alt="forma fondo blanco" className="w-full h-full absolute top-0" />
